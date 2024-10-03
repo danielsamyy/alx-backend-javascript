6663764528d9d0870174c02e8d651f36a0b7cd55
danielsamyy <danielsamy205@gmail.com>
2024-10-03 05:29:12 -0700

    0x06-unittests_in_js

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,29 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let sinonSpy;
+
+  beforeEach(() => {
+    if (!sinonSpy) {
+      sinonSpy = sinon.spy(console);
+    }
+  });
+
+  afterEach(() => {
+    sinonSpy.log.resetHistory();
+  });
+
+  it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
+    sendPaymentRequestToApi(100, 20);
+    expect(sinonSpy.log.calledWith('The total is: 120')).to.be.true;
+    expect(sinonSpy.log.calledOnce).to.be.true;
+  });
+
+  it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
+    sendPaymentRequestToApi(10, 10);
+    expect(sinonSpy.log.calledWith('The total is: 20')).to.be.true;
+    expect(sinonSpy.log.calledOnce).to.be.true;
+  });
+});
